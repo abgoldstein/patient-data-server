1439b53457c1007e2c4ec19b9a422527c5750a45
Dave Hill <dwhill@mitre.org>
2013-02-04 13:02:55 -0500

    Temporarily commented out CustomAuthFail::respond.

--- lib/devise/custom_auth_fail.rb
+++ lib/devise/custom_auth_fail.rb
@@ -2,21 +2,21 @@ class CustomAuthFail < Devise::FailureApp
   def respond
     super 
 
-    return if params.nil?
+    # return if params.nil?
 
 
-    login_email = ""
-    user = params[:user]
-    if !user.nil?
-      login_email = user[:email]
-    end
+    # login_email = ""
+    # user = params[:user]
+    # if !user.nil?
+    #   login_email = user[:email]
+    # end
 
-    if (login_email.length == 0)
-      ## user pressing sign-in with out entering email
-      AuditLog.create(requester_info: login_email, event: "user_auth_blank_login", description: "Blank Login")
-    else
-      AuditLog.create(requester_info: login_email, event: "user_auth_fail", description: "Login Failure")
-    end
+    # if (login_email.length == 0)
+    #   ## user pressing sign-in with out entering email
+    #   AuditLog.create(requester_info: login_email, event: "user_auth_blank_login", description: "Blank Login")
+    # else
+    #   AuditLog.create(requester_info: login_email, event: "user_auth_fail", description: "Login Failure")
+    # end
 
   end 
 end 
